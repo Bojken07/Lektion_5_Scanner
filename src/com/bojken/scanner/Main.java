e817137ea64171331d770af8fe0c4ce949faaa82
Bojken07 <bojken.zaimi@stud.sti.se>
2023-09-19 13:18:11 +0200

    scanner practice

--- src/com/bojken/scanner/Main.java
+++ src/com/bojken/scanner/Main.java
@@ -1,7 +1,43 @@
 package com.bojken.scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        boolean isPlaying = true;
+
+       do {
+
+         int result = scannerNumber();
+
+         for (int i = 0; i < result; i++){
+             System.out.println(i);
+             System.out.println("Looping...");
+         }
+
+          if (result == 1000) {
+              System.out.println("The number is 0");
+          }
+
+       } while (isPlaying);
+
+    }
+
+    public static int scannerNumber(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+
+         return scanner.nextInt();
+    }
+
+    public static String scannerText() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        String input = scanner.nextLine();
+
+        return input;
     }
 }
